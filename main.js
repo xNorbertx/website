3e00fc71001c490348cc00651e70550bbccefd69
Norbert Bakker <n.bakker@betabit.nl>
2019-01-09 08:44:59 +0100

    Scrolling carousel

--- main.js
+++ main.js
@@ -1,7 +1,58 @@
 const HEADER = document.getElementById('header');
+var CAROUSEL_IDX = 0;
+var CAROUSEL_TOTAL = $("div.slideshow-item").length;
 
+//Event: Navigation
 $("#nav-menu").children("a").click(navigate);
 
+
+$(function() {
+    const carousel = $("div.slideshow-wrapper ul");
+    const items = carousel.find("li.slideshow-item");
+    const itemWidth = carousel.find('li:first').width();
+    let clickCount = 0;
+
+    $("a.prev").click(moveLeft);
+    $("a.next").click(moveRight);
+
+    refreshChildPosition();
+
+    function moveLeft() {
+        clickCount--;
+    
+        //Find the first item and append it as the last item.
+        lastItem = carousel.find('li:last');
+        lastItem.remove().prependTo(carousel);
+    
+        lastItem.css('left', itemWidth*clickCount);				
+        //Animate the slider to right as item width 
+        carousel.finish().animate({
+            left: '+='+itemWidth
+        },300);
+    }
+    
+    function moveRight() {
+        clickCount++;
+            
+        //Animate the slider to left as item width 
+        carousel.finish().animate({
+            left : '-='+itemWidth
+        },300, function(){
+            //Find the first item and append it as the last item.
+            lastItem = carousel.find('li:first');
+            lastItem.remove().appendTo(carousel);
+            lastItem.css('left', ((items.length-1)*(itemWidth))+(clickCount*itemWidth));
+        });
+    }
+    
+    function refreshChildPosition(){
+        items.each(function(){
+            $(this).css('left', itemWidth*items.index($(this)));
+        });
+    }
+})
+
+//Event: Scroll
 window.onscroll = function() {
     scrollFunc()
 };
@@ -11,6 +62,7 @@ function navigate() {
     var y = $("#"+target).offset().top;
     window.scrollTo(0, y-90);
 }
+
 
 function scrollFunc() {
     //Set navbar position
